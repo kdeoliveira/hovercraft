eb8d97465279b830938acb33235bdd73b3f693fb
Kevin de Oliveira <contact@kevindeoliveira.com>
2022-02-28 22:47:06 -0500

    with ir and interrupt

--- assignment_1_uno/src/main.cpp
+++ assignment_1_uno/src/main.cpp
@@ -6,14 +6,20 @@ unsigned long recv_buf = 0;
 unsigned long length_mm = 0;
 
 #define TRIG_PIN 3
-#define ECHO_PIN 2
+#define ECHO_PIN 7
 #define LED_PIN 5
 #define LED_WARNING 4
+#define BTN_PIN 2
+#define IR_PIN A0
+volatile bool btn_state = false;
 
 
+void interrupt_change_state(void){
+  btn_state = !btn_state;
+}
 
-uint16_t high_pulse, low_pulse;
 
+int ir_data;
 
 void setup(void)
 {
@@ -23,31 +29,42 @@ void setup(void)
   pinMode(ECHO_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
   pinMode(LED_WARNING, OUTPUT);
+  pinMode(BTN_PIN, INPUT_PULLUP);
+  pinMode(IR_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(BTN_PIN), interrupt_change_state, FALLING);
 
 }
 
 void loop()
 {
-  Serial.flush();
+  if(btn_state){
+    ir_data = analogRead(IR_PIN);
+    Serial.println(106500.8 * pow(ir_data,-0.935) - 10);
 
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(50);
-  digitalWrite(TRIG_PIN, LOW);
+    delay(10);
 
-  recv_buf = pulseIn (ECHO_PIN, HIGH);
-  if(recv_buf > 1 && recv_buf < 60000){
-    length_mm = recv_buf*0.34/2;
-    // Serial.print("length (mm): ");
-    Serial.println(length_mm, DEC);
-    // Serial.print(hovercraft::whitespace);
-    // Serial.write(CARRIAGE_RETURN);
-    
+  }else{
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(50);
+    digitalWrite(TRIG_PIN, LOW);
 
-    led_value = hovercraft::map(length_mm, 150, 600,0, 255);
-    
-    analogWrite(LED_PIN, led_value);
+    recv_buf = pulseIn (ECHO_PIN, HIGH);
+    if(recv_buf > 1 && recv_buf < 60000){
+      length_mm = recv_buf*0.34/2;
+      // Serial.print("length (mm): ");
+      Serial.println(length_mm, DEC);
+      // Serial.print(hovercraft::whitespace);
+      // Serial.write(CARRIAGE_RETURN);
+      
 
-    digitalWrite(LED_WARNING, led_value == 0);
+      led_value = hovercraft::map(length_mm, 150, 600,0, 255);
+      
+      analogWrite(LED_PIN, led_value);
+
+      digitalWrite(LED_WARNING, led_value == 0);
+      delay(10);
+
+    }
   }
-  delay(10);
 }
+
