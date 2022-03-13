f6a706c5237b6f1425084c0a1eef163afb7f29bd
Kevin de Oliveira <contact@kevindeoliveira.com>
2022-03-13 19:59:12 -0400

    adjust LEDs

--- assignment_2/src/main.cpp
+++ assignment_2/src/main.cpp
@@ -4,23 +4,19 @@
 
 #define SerialDebug true
 
-// int intPin = 5; // These can be changed, 2 and 3 are the Arduinos ext int pins
-int myLed = 12;  // Set up pin 13 led for toggling
+#define LED_PIN 5
+#define LED_WARNING 12
 
 #define I2Cclock 400000
 #define I2Cport Wire
-#define MPU9250_ADDRESS MPU9250_ADDRESS_AD0 // Use either this line or the next to select which I2C address your device is using
-// #define MPU9250_ADDRESS MPU9250_ADDRESS_AD1
+#define MPU9250_ADDRESS MPU9250_ADDRESS_AD0
 
 MPU9250 myIMU(MPU9250_ADDRESS, I2Cport, I2Cclock);
 
+
+
 Servo myservo; // create servo object to control a servo
-// twelve servo objects can be created on most boards
 
-int pos = 0; // variable to store the servo position
-
-
-int vector_yaw_values[10];
 
 long map(long x, long min, long max, long out_min, long out_max)
 {
@@ -38,14 +34,19 @@ long map(long x, long min, long max, long out_min, long out_max)
 
 void setup()
 {
+
+
+
   Serial.begin(38400);
   myservo.attach(9); // attaches the servo on pin 9 to the servo object
   while (!Serial);
 
   Wire.begin();
 
-  pinMode(myLed, OUTPUT);
-  digitalWrite(myLed, HIGH);
+  pinMode(LED_WARNING, OUTPUT);
+
+  pinMode(LED_PIN, OUTPUT);
+
 
   byte c = myIMU.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
   Serial.print(F("MPU9250 I AM 0x"));
@@ -80,7 +81,22 @@ void setup()
 
     // Calibrate gyro and accelerometers, load biases in bias registers
     myIMU.calibrateMPU9250(myIMU.gyroBias, myIMU.accelBias);
+
+
+    // =====================
+    // Setting and initializing IMU
+    // =====================
+
+    //Setting the gyro full scale range
+    myIMU.Gscale = MPU9250::GFS_2000DPS; 
+    //Setting the accelerator full scale range
+    myIMU.Ascale = MPU9250::AFS_16G;
+
     myIMU.initMPU9250();
+
+
+
+
     // Initialize device for active mode read of acclerometer, gyroscope, and
     // temperature
     Serial.println("MPU9250 initialized for active data mode....");
@@ -123,6 +139,20 @@ void setup()
     myIMU.getGres();
     myIMU.getMres();
 
+    // =====================
+    // Setting and initializing IMU
+    // =====================
+
+    //Setting sample rate
+    //sample rates possible are 32 kHz (GYRO_CONFIG[1:0]=0x10), 8 kHz (CONFIG[2:0]=0x00), or 1 kHz (CONFIG[2:0]=0x03)
+    myIMU.writeByte(myIMU._I2Caddr, CONFIG, 0x03);
+    uint8_t gyro_config = myIMU.readByte(myIMU._I2Caddr,GYRO_CONFIG);
+    // F_CHOICE[1:0]
+    // 00 => Allow CONFIG TO BE READ
+    // 1x => Uses the GYRO_CONFIG [1:0] bits
+    gyro_config &= ~0x02;
+    myIMU.writeByte(myIMU._I2Caddr, GYRO_CONFIG, gyro_config);
+
 
 
     //    delay(2000); // Add delay to see results before serial spew of data
@@ -161,8 +191,7 @@ void setup()
   }
 }
 
-int val;
-char *buf;
+
 void loop()
 {
 
@@ -196,11 +225,10 @@ void loop()
   MahonyQuaternionUpdate(myIMU.ax, myIMU.ay, myIMU.az, myIMU.gx * DEG_TO_RAD,
                          myIMU.gy * DEG_TO_RAD, myIMU.gz * DEG_TO_RAD, myIMU.my,
                          myIMU.mx, -myIMU.mz, myIMU.deltat);
-
+  
   // CALCULATE
   myIMU.delt_t = millis() - myIMU.count;
 
-  // digitalWrite(myLed, digitalRead(myLed));
 
 
   myIMU.yaw = atan2(2.0f * (*(getQ() + 1) * *(getQ() + 2) + *getQ() * *(getQ() + 3)), *getQ() * *getQ() + *(getQ() + 1) * *(getQ() + 1) - *(getQ() + 2) * *(getQ() + 2) - *(getQ() + 3) * *(getQ() + 3));
@@ -208,26 +236,47 @@ void loop()
   myIMU.yaw *= RAD_TO_DEG;
   myIMU.yaw -= 8.5;
 
+  myIMU.pitch = -asin(2.0f * (*(getQ()+1) * *(getQ()+3) - *getQ()
+                * *(getQ()+2)));
+  myIMU.roll  = atan2(2.0f * (*getQ() * *(getQ()+1) + *(getQ()+2)
+                * *(getQ()+3)), *getQ() * *getQ() - *(getQ()+1)
+                * *(getQ()+1) - *(getQ()+2) * *(getQ()+2) + *(getQ()+3)
+                * *(getQ()+3));
+  myIMU.pitch *= RAD_TO_DEG;
+  myIMU.roll *= RAD_TO_DEG;
 
-  
+    analogWrite(LED_PIN, 255 - map(1000*abs(myIMU.ax), 1000*0.01, 1000, 0, 255));
+
   if(myIMU.yaw + 180 < 180 || myIMU.yaw + 180 > 0){
     int result_servo = (int)map( floor( myIMU.yaw) + 180, 0, 180, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
     if(result_servo == 0){
-        digitalWrite(myLed, LOW);
+        digitalWrite(LED_WARNING, LOW);
         Serial.print("Out of bound\r");
     }else{
-      digitalWrite(myLed, HIGH);
+      digitalWrite(LED_WARNING, HIGH);
       myservo.writeMicroseconds( result_servo);
-      delay(10);
     }
   }
-  
-  if (myIMU.delt_t > 500)
+
+  if (myIMU.delt_t > 1000) //1s for serial print
   {
     if (SerialDebug)
     {
-      Serial.print("Yaw: ");
+      Serial.print("ax = ");  
+      Serial.print((int)1000* myIMU.ax);
+      Serial.print(" mg  [");
+
+      Serial.print(myIMU.ax, 4);
+      Serial.print("g]");
+
+      Serial.print("\tYaw: ");
       Serial.print(myIMU.yaw + 180, 2);
+
+      Serial.print("\tPitch: ");
+      Serial.print(myIMU.pitch, 2);
+
+      Serial.print("\tRoll: ");
+      Serial.print(myIMU.roll, 2);
 
       Serial.print("\trate = ");
       Serial.print((float)myIMU.sumCount / myIMU.sum, 2);
