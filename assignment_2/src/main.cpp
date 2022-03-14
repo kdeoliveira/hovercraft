67919a96ea0cd334024896bb9697a34dadde10c3
Kevin de Oliveira <contact@kevindeoliveira.com>
2022-03-13 20:21:28 -0400

    util class

--- assignment_2/src/main.cpp
+++ assignment_2/src/main.cpp
@@ -1,6 +1,6 @@
 #include "quaternionFilters.h"
-#include "MPU9250.h"
 #include <Servo.h>
+#include <util.hpp>
 
 #define SerialDebug true
 
@@ -11,31 +11,12 @@
 #define I2Cport Wire
 #define MPU9250_ADDRESS MPU9250_ADDRESS_AD0
 
-MPU9250 myIMU(MPU9250_ADDRESS, I2Cport, I2Cclock);
-
-
+MPU9250 mpu_imu(MPU9250_ADDRESS, I2Cport, I2Cclock);
 
 Servo myservo; // create servo object to control a servo
 
-
-long map(long x, long min, long max, long out_min, long out_max)
-{
-  if (x <= min)
-  {
-    return 0;
-  }
-  else if (x >= max)
-  {
-    return 0;
-  }
-
-  return (x - min) * (out_max - out_min) / (max - min) + out_min;
-}
-
 void setup()
 {
-
-
 
   Serial.begin(38400);
   myservo.attach(9); // attaches the servo on pin 9 to the servo object
@@ -47,8 +28,7 @@ void setup()
 
   pinMode(LED_PIN, OUTPUT);
 
-
-  byte c = myIMU.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
+  byte c = mpu_imu.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
   Serial.print(F("MPU9250 I AM 0x"));
   Serial.print(c, HEX);
   Serial.print(F(" I should be 0x"));
@@ -58,44 +38,21 @@ void setup()
   {
     Serial.println(F("MPU9250 is online..."));
 
-    // Start by performing self test and reporting values
-    myIMU.MPU9250SelfTest(myIMU.selfTest);
-    Serial.print(F("x-axis self test: acceleration trim within : "));
-    Serial.print(myIMU.selfTest[0], 1);
-    Serial.println("% of factory value");
-    Serial.print(F("y-axis self test: acceleration trim within : "));
-    Serial.print(myIMU.selfTest[1], 1);
-    Serial.println("% of factory value");
-    Serial.print(F("z-axis self test: acceleration trim within : "));
-    Serial.print(myIMU.selfTest[2], 1);
-    Serial.println("% of factory value");
-    Serial.print(F("x-axis self test: gyration trim within : "));
-    Serial.print(myIMU.selfTest[3], 1);
-    Serial.println("% of factory value");
-    Serial.print(F("y-axis self test: gyration trim within : "));
-    Serial.print(myIMU.selfTest[4], 1);
-    Serial.println("% of factory value");
-    Serial.print(F("z-axis self test: gyration trim within : "));
-    Serial.print(myIMU.selfTest[5], 1);
-    Serial.println("% of factory value");
+    print_self_test_imu(mpu_imu);
 
     // Calibrate gyro and accelerometers, load biases in bias registers
-    myIMU.calibrateMPU9250(myIMU.gyroBias, myIMU.accelBias);
-
+    mpu_imu.calibrateMPU9250(mpu_imu.gyroBias, mpu_imu.accelBias);
 
     // =====================
     // Setting and initializing IMU
     // =====================
 
-    //Setting the gyro full scale range
-    myIMU.Gscale = MPU9250::GFS_2000DPS; 
-    //Setting the accelerator full scale range
-    myIMU.Ascale = MPU9250::AFS_16G;
+    // Setting the gyro full scale range
+    mpu_imu.Gscale = MPU9250::GFS_2000DPS;
+    // Setting the accelerator full scale range
+    mpu_imu.Ascale = MPU9250::AFS_16G;
 
-    myIMU.initMPU9250();
-
-
-
+    mpu_imu.initMPU9250();
 
     // Initialize device for active mode read of acclerometer, gyroscope, and
     // temperature
@@ -103,7 +60,7 @@ void setup()
 
     // Read the WHO_AM_I register of the magnetometer, this is a good test of
     // communication
-    byte d = myIMU.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963);
+    byte d = mpu_imu.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963);
     Serial.print("AK8963 ");
     Serial.print("I AM 0x");
     Serial.print(d, HEX);
@@ -119,7 +76,7 @@ void setup()
     }
 
     // Get magnetometer calibration from AK8963 ROM
-    myIMU.initAK8963(myIMU.factoryMagCalibration);
+    mpu_imu.initAK8963(mpu_imu.factoryMagCalibration);
     // Initialize device for active mode read of magnetometer
     Serial.println("AK8963 initialized for active data mode....");
 
@@ -127,55 +84,35 @@ void setup()
     {
       //  Serial.println("Calibration values: ");
       Serial.print("X-Axis factory sensitivity adjustment value ");
-      Serial.println(myIMU.factoryMagCalibration[0], 2);
+      Serial.println(mpu_imu.factoryMagCalibration[0], 2);
       Serial.print("Y-Axis factory sensitivity adjustment value ");
-      Serial.println(myIMU.factoryMagCalibration[1], 2);
+      Serial.println(mpu_imu.factoryMagCalibration[1], 2);
       Serial.print("Z-Axis factory sensitivity adjustment value ");
-      Serial.println(myIMU.factoryMagCalibration[2], 2);
+      Serial.println(mpu_imu.factoryMagCalibration[2], 2);
     }
 
     // Get sensor resolutions, only need to do this once
-    myIMU.getAres();
-    myIMU.getGres();
-    myIMU.getMres();
+    mpu_imu.getAres();
+    mpu_imu.getGres();
+    mpu_imu.getMres();
 
     // =====================
     // Setting and initializing IMU
     // =====================
 
-    //Setting sample rate
-    //sample rates possible are 32 kHz (GYRO_CONFIG[1:0]=0x10), 8 kHz (CONFIG[2:0]=0x00), or 1 kHz (CONFIG[2:0]=0x03)
-    myIMU.writeByte(myIMU._I2Caddr, CONFIG, 0x03);
-    uint8_t gyro_config = myIMU.readByte(myIMU._I2Caddr,GYRO_CONFIG);
+    // Setting sample rate
+    // sample rates possible are 32 kHz (GYRO_CONFIG[1:0]=0x10), 8 kHz (CONFIG[2:0]=0x00), or 1 kHz (CONFIG[2:0]=0x03)
+    mpu_imu.writeByte(mpu_imu._I2Caddr, CONFIG, 0x06);
+    uint8_t gyro_config = mpu_imu.readByte(mpu_imu._I2Caddr, GYRO_CONFIG);
     // F_CHOICE[1:0]
     // 00 => Allow CONFIG TO BE READ
     // 1x => Uses the GYRO_CONFIG [1:0] bits
     gyro_config &= ~0x02;
-    myIMU.writeByte(myIMU._I2Caddr, GYRO_CONFIG, gyro_config);
-
-
-
-    //    delay(2000); // Add delay to see results before serial spew of data
+    mpu_imu.writeByte(mpu_imu._I2Caddr, GYRO_CONFIG, gyro_config);
 
     if (SerialDebug)
     {
-          Serial.println("AK8963 mag biases (mG)");
-    Serial.println(myIMU.magBias[0]);
-    Serial.println(myIMU.magBias[1]);
-    Serial.println(myIMU.magBias[2]);
-
-    Serial.println("AK8963 mag scale (mG)");
-    Serial.println(myIMU.magScale[0]);
-    Serial.println(myIMU.magScale[1]);
-    Serial.println(myIMU.magScale[2]);
-
-      Serial.println("Magnetometer:");
-      Serial.print("X-Axis sensitivity adjustment value ");
-      Serial.println(myIMU.factoryMagCalibration[0], 2);
-      Serial.print("Y-Axis sensitivity adjustment value ");
-      Serial.println(myIMU.factoryMagCalibration[1], 2);
-      Serial.print("Z-Axis sensitivity adjustment value ");
-      Serial.println(myIMU.factoryMagCalibration[2], 2);
+      print_info_imu(mpu_imu);
     }
 
   } // if (c == 0x71)
@@ -191,102 +128,104 @@ void setup()
   }
 }
 
-
 void loop()
 {
 
-  
-
-
-  if (myIMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
+  if (mpu_imu.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
   {
-    myIMU.readAccelData(myIMU.accelCount); // Read the x/y/z adc values
+    mpu_imu.readAccelData(mpu_imu.accelCount); // Read the x/y/z adc values
 
-    myIMU.ax = (float)myIMU.accelCount[0] * myIMU.aRes; // - myIMU.accelBias[0];
-    myIMU.ay = (float)myIMU.accelCount[1] * myIMU.aRes; // - myIMU.accelBias[1];
-    myIMU.az = (float)myIMU.accelCount[2] * myIMU.aRes; // - myIMU.accelBias[2];
+    mpu_imu.ax = (float)mpu_imu.accelCount[0] * mpu_imu.aRes; // - mpu_imu.accelBias[0];
+    mpu_imu.ay = (float)mpu_imu.accelCount[1] * mpu_imu.aRes; // - mpu_imu.accelBias[1];
+    mpu_imu.az = (float)mpu_imu.accelCount[2] * mpu_imu.aRes; // - mpu_imu.accelBias[2];
 
-    myIMU.readGyroData(myIMU.gyroCount); // Read the x/y/z adc values
+    mpu_imu.readGyroData(mpu_imu.gyroCount); // Read the x/y/z adc values
 
     // Calculate the gyro value into actual degrees per second
     // This depends on scale being set
-    myIMU.gx = (float)myIMU.gyroCount[0] * myIMU.gRes;
-    myIMU.gy = (float)myIMU.gyroCount[1] * myIMU.gRes;
-    myIMU.gz = (float)myIMU.gyroCount[2] * myIMU.gRes;
+    mpu_imu.gx = (float)mpu_imu.gyroCount[0] * mpu_imu.gRes;
+    mpu_imu.gy = (float)mpu_imu.gyroCount[1] * mpu_imu.gRes;
+    mpu_imu.gz = (float)mpu_imu.gyroCount[2] * mpu_imu.gRes;
 
-    myIMU.readMagData(myIMU.magCount); // Read the x/y/z adc values
+    mpu_imu.readMagData(mpu_imu.magCount); // Read the x/y/z adc values
 
-    myIMU.mx = (float)myIMU.magCount[0] * myIMU.mRes * myIMU.factoryMagCalibration[0] - myIMU.magBias[0];
-    myIMU.my = (float)myIMU.magCount[1] * myIMU.mRes * myIMU.factoryMagCalibration[1] - myIMU.magBias[1];
-    myIMU.mz = (float)myIMU.magCount[2] * myIMU.mRes * myIMU.factoryMagCalibration[2] - myIMU.magBias[2];
+    mpu_imu.mx = (float)mpu_imu.magCount[0] * mpu_imu.mRes * mpu_imu.factoryMagCalibration[0] - mpu_imu.magBias[0];
+    mpu_imu.my = (float)mpu_imu.magCount[1] * mpu_imu.mRes * mpu_imu.factoryMagCalibration[1] - mpu_imu.magBias[1];
+    mpu_imu.mz = (float)mpu_imu.magCount[2] * mpu_imu.mRes * mpu_imu.factoryMagCalibration[2] - mpu_imu.magBias[2];
   }
-  myIMU.updateTime();
-  //https://forum.arduino.cc/t/fatal-bug-in-sparkfun-and-kris-winers-mpu9250-code/619810
-  MahonyQuaternionUpdate(myIMU.ax, myIMU.ay, myIMU.az, myIMU.gx * DEG_TO_RAD,
-                         myIMU.gy * DEG_TO_RAD, myIMU.gz * DEG_TO_RAD, myIMU.my,
-                         myIMU.mx, -myIMU.mz, myIMU.deltat);
-  
+  mpu_imu.updateTime();
+  // https://forum.arduino.cc/t/fatal-bug-in-sparkfun-and-kris-winers-mpu9250-code/619810
+  MahonyQuaternionUpdate(mpu_imu.ax, mpu_imu.ay, mpu_imu.az, mpu_imu.gx * DEG_TO_RAD,
+                         mpu_imu.gy * DEG_TO_RAD, mpu_imu.gz * DEG_TO_RAD, mpu_imu.my,
+                         mpu_imu.mx, -mpu_imu.mz, mpu_imu.deltat);
+
   // CALCULATE
-  myIMU.delt_t = millis() - myIMU.count;
+  mpu_imu.delt_t = millis() - mpu_imu.count;
 
+  mpu_imu.yaw = atan2(2.0f * (*(getQ() + 1) * *(getQ() + 2) + *getQ() * *(getQ() + 3)), *getQ() * *getQ() + *(getQ() + 1) * *(getQ() + 1) - *(getQ() + 2) * *(getQ() + 2) - *(getQ() + 3) * *(getQ() + 3));
 
+  mpu_imu.yaw *= RAD_TO_DEG;
+  mpu_imu.yaw -= 8.5;
 
-  myIMU.yaw = atan2(2.0f * (*(getQ() + 1) * *(getQ() + 2) + *getQ() * *(getQ() + 3)), *getQ() * *getQ() + *(getQ() + 1) * *(getQ() + 1) - *(getQ() + 2) * *(getQ() + 2) - *(getQ() + 3) * *(getQ() + 3));
+  mpu_imu.pitch = -asin(2.0f * (*(getQ() + 1) * *(getQ() + 3) - *getQ() * *(getQ() + 2)));
+  mpu_imu.roll = atan2(2.0f * (*getQ() * *(getQ() + 1) + *(getQ() + 2) * *(getQ() + 3)), *getQ() * *getQ() - *(getQ() + 1) * *(getQ() + 1) - *(getQ() + 2) * *(getQ() + 2) + *(getQ() + 3) * *(getQ() + 3));
+  mpu_imu.pitch *= RAD_TO_DEG;
+  mpu_imu.roll *= RAD_TO_DEG;
 
-  myIMU.yaw *= RAD_TO_DEG;
-  myIMU.yaw -= 8.5;
+  analogWrite(LED_PIN, 255 - map(1000 * abs(mpu_imu.ax), 1000 * 0.01, 1000, 0, 255));
 
-  myIMU.pitch = -asin(2.0f * (*(getQ()+1) * *(getQ()+3) - *getQ()
-                * *(getQ()+2)));
-  myIMU.roll  = atan2(2.0f * (*getQ() * *(getQ()+1) + *(getQ()+2)
-                * *(getQ()+3)), *getQ() * *getQ() - *(getQ()+1)
-                * *(getQ()+1) - *(getQ()+2) * *(getQ()+2) + *(getQ()+3)
-                * *(getQ()+3));
-  myIMU.pitch *= RAD_TO_DEG;
-  myIMU.roll *= RAD_TO_DEG;
-
-    analogWrite(LED_PIN, 255 - map(1000*abs(myIMU.ax), 1000*0.01, 1000, 0, 255));
-
-  if(myIMU.yaw + 180 < 180 || myIMU.yaw + 180 > 0){
-    int result_servo = (int)map( floor( myIMU.yaw) + 180, 0, 180, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
-    if(result_servo == 0){
-        digitalWrite(LED_WARNING, LOW);
-        Serial.print("Out of bound\r");
-    }else{
+  if (mpu_imu.yaw + 180 < 180 || mpu_imu.yaw + 180 > 0)
+  {
+    int result_servo = (int)map(floor(mpu_imu.yaw) + 180, 0, 180, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
+    if (result_servo == 0)
+    {
+      digitalWrite(LED_WARNING, LOW);
+      Serial.print("Out of bound\r");
+    }
+    else
+    {
       digitalWrite(LED_WARNING, HIGH);
-      myservo.writeMicroseconds( result_servo);
+      myservo.writeMicroseconds(result_servo);
     }
   }
 
-  if (myIMU.delt_t > 1000) //1s for serial print
+  if (mpu_imu.delt_t > 1000) // 1s for serial print
   {
     if (SerialDebug)
     {
-      Serial.print("ax = ");  
-      Serial.print((int)1000* myIMU.ax);
+      Serial.print("CONFIG register: ");
+      Serial.print(
+          mpu_imu.readByte(mpu_imu._I2Caddr, CONFIG), BIN);
+
+      Serial.print(" - ");
+      Serial.print("GYRO_CONFIG register: ");
+      Serial.print(
+          mpu_imu.readByte(mpu_imu._I2Caddr, GYRO_CONFIG), BIN);
+      Serial.print(
+          " : ");
+      Serial.print("ax = ");
+      Serial.print((int)1000 * mpu_imu.ax);
       Serial.print(" mg  [");
 
-      Serial.print(myIMU.ax, 4);
+      Serial.print(mpu_imu.ax, 4);
       Serial.print("g]");
 
       Serial.print("\tYaw: ");
-      Serial.print(myIMU.yaw + 180, 2);
+      Serial.print(mpu_imu.yaw + 180, 2);
 
       Serial.print("\tPitch: ");
-      Serial.print(myIMU.pitch, 2);
+      Serial.print(mpu_imu.pitch, 2);
 
       Serial.print("\tRoll: ");
-      Serial.print(myIMU.roll, 2);
+      Serial.print(mpu_imu.roll, 2);
 
       Serial.print("\trate = ");
-      Serial.print((float)myIMU.sumCount / myIMU.sum, 2);
+      Serial.print((float)mpu_imu.sumCount / mpu_imu.sum, 2);
       Serial.println(" Hz");
 
-      
-
-      myIMU.count = millis();
-      myIMU.sumCount = 0;
-      myIMU.sum = 0;
+      mpu_imu.count = millis();
+      mpu_imu.sumCount = 0;
+      mpu_imu.sum = 0;
     }
   }
 }
---
+++ assignment_2/src/util.hpp
@@ -0,0 +1,65 @@
+#pragma once
+#include "MPU9250.h"
+
+
+long map(long x, long min, long max, long out_min, long out_max)
+{
+  if (x <= min)
+  {
+    return 0;
+  }
+  else if (x >= max)
+  {
+    return 0;
+  }
+
+  return (x - min) * (out_max - out_min) / (max - min) + out_min;
+}
+
+
+
+
+void print_info_imu(MPU9250 imu){
+              Serial.println("AK8963 mag biases (mG)");
+    Serial.println(imu.magBias[0]);
+    Serial.println(imu.magBias[1]);
+    Serial.println(imu.magBias[2]);
+
+    Serial.println("AK8963 mag scale (mG)");
+    Serial.println(imu.magScale[0]);
+    Serial.println(imu.magScale[1]);
+    Serial.println(imu.magScale[2]);
+
+      Serial.println("Magnetometer:");
+      Serial.print("X-Axis sensitivity adjustment value ");
+      Serial.println(imu.factoryMagCalibration[0], 2);
+      Serial.print("Y-Axis sensitivity adjustment value ");
+      Serial.println(imu.factoryMagCalibration[1], 2);
+      Serial.print("Z-Axis sensitivity adjustment value ");
+      Serial.println(imu.factoryMagCalibration[2], 2);
+}
+
+
+
+void print_self_test_imu(MPU9250 imu){
+        // Start by performing self test and reporting values
+    imu.MPU9250SelfTest(imu.selfTest);
+    Serial.print(F("x-axis self test: acceleration trim within : "));
+    Serial.print(imu.selfTest[0], 1);
+    Serial.println("% of factory value");
+    Serial.print(F("y-axis self test: acceleration trim within : "));
+    Serial.print(imu.selfTest[1], 1);
+    Serial.println("% of factory value");
+    Serial.print(F("z-axis self test: acceleration trim within : "));
+    Serial.print(imu.selfTest[2], 1);
+    Serial.println("% of factory value");
+    Serial.print(F("x-axis self test: gyration trim within : "));
+    Serial.print(imu.selfTest[3], 1);
+    Serial.println("% of factory value");
+    Serial.print(F("y-axis self test: gyration trim within : "));
+    Serial.print(imu.selfTest[4], 1);
+    Serial.println("% of factory value");
+    Serial.print(F("z-axis self test: gyration trim within : "));
+    Serial.print(imu.selfTest[5], 1);
+    Serial.println("% of factory value");
+}
