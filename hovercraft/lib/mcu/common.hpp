4c80627969412d2232fab2e9535c17d0d52f6f89
Kevin de Oliveira <contact@kevindeoliveira.com>
2022-04-08 19:45:27 -0400

    state machine

--- hovercraft/lib/mcu/common.hpp
+++ hovercraft/lib/mcu/common.hpp
@@ -1,32 +1,119 @@
+#pragma once
 
-
-#define LIFT_FAN        6
-#define THRUST_FAN      10
-#define BATTERY_STATUS  A6
-#define SERVO           9
-#define LED_GREEN       5
-#define LED_WARNING     13
+#define LIFT_FAN 6
+#define THRUST_FAN 10
+#define BATTERY_STATUS A6
+#define SERVO 9
+#define LED_GREEN 5
+#define LED_WARNING 13
 // PD3 - 1x3
-#define US_SENSOR_1     3
+#define US_SENSOR_1 3
 // PD4 - 1x2
-#define US_SENSOR_2     4
+#define US_SENSOR_2 4
 // PD2 - 2x3
-#define US_SENSOR_3     2
+#define US_SENSOR_3 2
 
+namespace hc
+{
 
-
-
-namespace hc{
-
-    enum STATE : int{
+    enum class STATE : int
+    {
+        START,
         LEFT,
         FRONT,
-        RIGHT
+        RIGHT,
+        END
     };
 
+    struct int_cycle
+    {
+        int cycles = 0;
 
+        int_cycle &operator++()
+        {
+            (*this).cycles = ((*this).cycles + 1) % 5;
+            return *this;
+        }
 
+        int_cycle operator++(int)
+        {
+            (*this).cycles = ((*this).cycles + 1) % 5;
+            return *this;
+        }
 
-    
+        int_cycle &operator--()
+        {
+            if ((*this).cycles == 0 ) return *this;
+            (*this).cycles = ((*this).cycles - 1) % 5;
+            return *this;
+        }
+
+        int_cycle operator--(int)
+        {
+            if ((*this).cycles == 0 ) return *this;
+            (*this).cycles = ((*this).cycles - 1) % 5;
+            return *this;
+        }
+    };
+
+    class StateMachine
+    {
+
+    private:
+        STATE m_state;
+        int_cycle m_type;
+        double m_transition_value;
+
+    public:
+        StateMachine(double transition) : m_transition_value{transition}
+        {
+            m_state = STATE(m_type.cycles);
+        }
+
+        STATE run(double sensor_left, double sensor_front, double sensor_right)
+        {
+            if (m_type.cycles == 0)
+            {
+                ++m_type;
+                return this->m_state = STATE(m_type.cycles);
+            }
+
+            else if (m_type.cycles == 1)
+            {
+                if (sensor_left < m_transition_value)
+                {
+                    ++m_type;
+                    return m_state = STATE(m_type.cycles);
+                }
+            }
+            else if (m_type.cycles == 2)
+            {
+                if (sensor_front < m_transition_value && sensor_left > m_transition_value)
+                {
+                    --m_type;
+                    return m_state = STATE(m_type.cycles);
+                }
+                else if (sensor_front < m_transition_value && sensor_right > m_transition_value)
+                {
+                    ++m_type;
+                    return m_state = STATE(m_type.cycles);
+                }
+            }
+            else if (m_type.cycles == 3)
+            {
+                if (sensor_right > m_transition_value)
+                {
+                    --m_type;
+                    return m_state = STATE(m_type.cycles);
+                }
+                else if (sensor_right < m_transition_value)
+                {
+                    ++m_type;
+                    return this->m_state = STATE(m_type.cycles);
+                }
+            }
+            return this->m_state;
+        }
+    };
 
 }
--- hovercraft/src/main.cpp
+++ hovercraft/src/main.cpp
@@ -1,81 +1,61 @@
 #include <Arduino.h>
 
 #include <common.hpp>
-#include <actuator.hpp>
 #include <Servo.h>
 #include <util.hpp>
 
-#include <AutoPID.h>
+#include <PID_v1.h>
 
 // Declared weak in Arduino.h to allow user redefinitions.
-int atexit(void (* /*func*/ )()) { return 0; }
+int atexit(void (*/*func*/)()) { return 0; }
 
 // Weak empty variant initialization function.
 // May be redefined by variant files.
 void initVariant() __attribute__((weak));
-void initVariant() { }
+void initVariant() {}
 
 void setupUSB() __attribute__((weak));
-void setupUSB() { }
+void setupUSB() {}
 
-
-
-
-
-
-const int min_us_sensor = 20;
 
 
 #define US_SENSOR_LEFT US_SENSOR_1
 #define US_SENSOR_RIGHT US_SENSOR_2
 #define US_SENSOR_FRONT US_SENSOR_3
 
-
 int main(void)
 {
-	init();
+    init();
 
-	initVariant();
+    initVariant();
 
 #if defined(USBCON)
-	USBDevice.attach();
+    USBDevice.attach();
 #endif
-	
+
     /**
      * @brief Begin of setup()
-     * 
+     *
      */
 
     Serial.begin(9600);
 
-    hc::Actuator us_sensor_controller{5,2,1};
 
-    double distance_left, distance_front, distance_right, distance_output_left;
-    double set_point;
+    double set_point, input, output;
+    PID pid_controller(&input, &output, &set_point, 0,10,0, P_ON_M, DIRECT);
+    set_point = 22.0;
+    pid_controller.SetOutputLimits(0, 255);
+    pid_controller.SetMode(AUTOMATIC);
 
+    unsigned long us_sensor_buffer[3];
 
-    AutoPID pid_controller(&distance_left, &set_point, &distance_output_left, 0, 255, 0.12, 0.0003, 0);
-
-    pid_controller.setBangBang(4.5);
-
-    pid_controller.setTimeStep(1000);
-
-    set_point = 20.0;
+    double distance_left, distance_front, distance_right;
 
     Servo servo_motor;
 
     servo_motor.attach(SERVO);
 
-
-    // hc::STATE current_state = hc::STATE::LEFT;
-    // 0 : left
-    // 1 : front
-    // 2 : right
-    int current_state = 0;
-
-
-
-    
+    hc::StateMachine state_machine(20.0);
 
     pinMode(LIFT_FAN, OUTPUT);
     pinMode(THRUST_FAN, OUTPUT);
@@ -84,35 +64,35 @@ int main(void)
     pinMode(US_SENSOR_RIGHT, INPUT);
     pinMode(US_SENSOR_FRONT, INPUT);
 
-
     pinMode(LED_GREEN, OUTPUT);
     pinMode(SERVO, OUTPUT);
     pinMode(LED_WARNING, OUTPUT);
 
-    unsigned long us_sensor_buffer[3];
 
-    int i = 0;
 
-    
-	while(true){
+    while (true)
+    {
         // servo_motor.writeMicroseconds(MIN_PULSE_WIDTH);
 
         analogWrite(LIFT_FAN, 255);
         analogWrite(THRUST_FAN, 0);
-		
+
         us_sensor_buffer[0] = pulseIn(US_SENSOR_LEFT, HIGH);
         us_sensor_buffer[1] = pulseIn(US_SENSOR_FRONT, HIGH);
         us_sensor_buffer[2] = pulseIn(US_SENSOR_RIGHT, HIGH);
 
-        if( (us_sensor_buffer[0] > 0) && (us_sensor_buffer[0] <= 37500) ){
+        if ((us_sensor_buffer[0] > 0) && (us_sensor_buffer[0] <= 37500))
+        {
             distance_left = us_sensor_buffer[0] / 147 * 2.54;
         }
 
-        if( (us_sensor_buffer[1] > 0) && (us_sensor_buffer[1] <= 37500) ){
+        if ((us_sensor_buffer[1] > 0) && (us_sensor_buffer[1] <= 37500))
+        {
             distance_front = us_sensor_buffer[1] / 147 * 2.54;
         }
 
-        if( (us_sensor_buffer[2] > 0) && (us_sensor_buffer[2] <= 37500) ){
+        if ((us_sensor_buffer[2] > 0) && (us_sensor_buffer[2] <= 37500))
+        {
             distance_right = us_sensor_buffer[2] / 147 * 2.54;
         }
 
@@ -123,100 +103,43 @@ int main(void)
         // Serial.print("\tRIGHT: ");
         // Serial.println(distance_right);
 
-        
-        
-        
-        if(
-            (distance_left - distance_front) > 0 && (distance_left - distance_right) > 0
-        ){
+        hc::STATE current_state = state_machine.run(distance_left, distance_front, distance_right);
+
+        if (current_state == hc::STATE::LEFT)
+        {
+
+            input = distance_front;
+            // input =  hc::Util::map(distance_front, 13.0, 40.0, 0, 255);
+            pid_controller.Compute();
 
             servo_motor.writeMicroseconds(
-                MIN_PULSE_WIDTH
+                hc::Util::map(output, 0, 255, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH)
             );
-            
+
             Serial.println("LEFT");
             analogWrite(THRUST_FAN, 255);
-
         }
-        else if(
-            (distance_front - distance_left) > 0 && (distance_front - distance_right) > 0
-        ){
+        else if (current_state == hc::STATE::FRONT)
+        {
             servo_motor.writeMicroseconds(
-                (MIN_PULSE_WIDTH + MAX_PULSE_WIDTH) /2
-            );
+                (MIN_PULSE_WIDTH + MAX_PULSE_WIDTH) / 2);
 
             Serial.println("FRONT");
             analogWrite(THRUST_FAN, 255);
         }
-        else if(
-            (distance_right - distance_front) > 0 && (distance_right - distance_left) > 0
-        ){
-            servo_motor.writeMicroseconds(
-                MAX_PULSE_WIDTH
-            );
+
+        else if (current_state == hc::STATE::RIGHT)
+        {
+            servo_motor.writeMicroseconds(MAX_PULSE_WIDTH);
             Serial.println("RIGHT");
             analogWrite(THRUST_FAN, 255);
         }
 
-
-
-        
-
-
-
-        // if( (us_sensor_buffer[0] > 0) && (us_sensor_buffer[0] <= 37500) ){
-        //     distance_left = us_sensor_buffer[0] / 147 * 2.54;
-        //     Serial.print(distance_left);
-
-        //     pid_controller.run();
-            
-        //     Serial.print("\t|\t");
-        //     if(pid_controller.atSetPoint(1)) continue;
-
-        //     if(distance_output_left > 0 && distance_left - 20.0 < 0){
-        //         int servo_angle = hc::Util::map(abs(distance_left - 20.0), 0, 5, MIN_PULSE_WIDTH, (MIN_PULSE_WIDTH+MAX_PULSE_WIDTH)/3);
-        //         Serial.print(servo_angle);
-        //         servo_motor.writeMicroseconds(servo_angle);
-        //     }
-
-            //  analogWrite(LED_WARNING, pid_controller.atSetPoint(1));
-            //  Serial.print("\t|\t");
-
-            //  Serial.println(distance_output_left);
-
-            
-            // us_sensor_controller.start(distance);
-            
-            
-        // }
-
-
-
-        
-        // int servo_angle = hc::Util::map(i, 0, 255, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
-        // servo_motor.writeMicroseconds(MIN_PULSE_WIDTH);
-        
-        
-
-        // i  = (i +  1) % 255;
-
-        // if(us_sensor_controller.output_signal(millis())){
-        //     analogWrite(LED_GREEN, 255);
-        // }else{
-        //     analogWrite(LED_GREEN, 0);
-        // }
-        
-        // Serial.println((us_sensor_controller.get_output()));
-
-
-
-
-
-
-		if (serialEventRun) serialEventRun();
+        if (serialEventRun)
+            serialEventRun();
 
         delay(10);
-	}
-        
-	return 0;
+    }
+
+    return 0;
 }
